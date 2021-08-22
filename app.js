cc8b760758f310897897f8cc2e73f1d650dd8e0f
devandtravel <devandtravel@gmail.com>
2021-08-22 16:05:17 +0300

    add: v-for, v-if, v-show

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const App = {
   data() {
     return {
       customHTML: '<h1>Custom HTML header</h1>',
-      title: 'Header'
+      title: 'Header',
+      person: {
+        firstName: 'devandtravel',
+        github: 'https://github.com/devandtravel',
+        interest: 'cats'
+      },
+      items: [1, 2, 3, 4, 5]
     }
   },
   methods: {
